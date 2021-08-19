70f734abcbdb382255887c3c9f87dcf0958d2eab
yeony10 <yeony10@github.com>
2021-08-20 02:41:42 +0900

    chore #16 - 테스트를 위한 HTTP GET API 및 테스트 코드 작성

--- src/main/java/com/readers/jikji/web/HelloController.java
+++ src/main/java/com/readers/jikji/web/HelloController.java
@@ -1,2 +1,13 @@
-package com.readers.jikji.web;public class HelloController {
+package com.readers.jikji.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
