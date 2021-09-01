4416eaca26ecbbc3722efde3793eaa0c1cd933e3
devminseo <dev.minseo@gmail.com>
2021-09-01 21:56:30 +0900

    feat #17 - topbar, navbar 화면 구현 및 components 파일만 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import axios from 'axios';
 
-function App() {
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from '@material-ui/core/Button';
+
+/* function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header/ >
+    // metarial ui 실습코드 필요시 참고해서 쓰기
+    // <Button variant="contained" color="secondary">Hello!</Button>
   );
+} */
+
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+  
+  test = async () => {
+    const tests = await axios.get('http://ec2-15-165-84-47.ap-northeast-2.compute.amazonaws.com:7000/hello');
+  }
+  componentDidMount() {
+    this.test();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...': 'Ready'}</div>
+  }
 }
 
-export default App;
+export default App; 
