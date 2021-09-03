10b4ff263fec79ffe6c8380e8f12c8cdba44409d
devminseo <dev.minseo@gmail.com>
2021-09-04 01:48:22 +0900

    feat #21 - 업데이트 책 목록 구현(미완), 네비게이션바 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,74 @@
-import React, {Fragment} from 'react';
-import axios from 'axios';
+/*eslint-disable*/
+
+import React, {useState} from 'react';
 import './App.css';
-import Header from './components/Header';
-//import Footer from './components/Footer';
+import { NavLink } from 'react-bootstrap';
+import Newbook from './Newbook.js';
+// import axios from 'axios';
+
 import dotenv from "dotenv";
 dotenv.config();
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Join from './components/Join';
-import Login from './components/Login';
-import Mypage from './components/Mypage';
-import Search from './components/Search';
-import Viewer from './components/Viewer';
-import Statistics from './components/Statistics';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from '@material-ui/core/Button';
 
 function App() {
+
+  let[books, changeBooks] = useState(Newbook);
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path={"/join"} component={Join} />
-        <Route path={"/login"} component={Login} />
-        <Route path={"/mypage"} component={Mypage} />
-        <Route path={"/search"} component={Search} />
-        <Route path={"/viewer"} component={Viewer} />
-        <Route path={"/statistics"} component={Statistics} />
-      </Switch>
-    </Router>
-    
-    // metarial ui 실습코드 필요시 참고해서 쓰기
-    // <Button variant="contained" color="secondary">Hello!</Button>
+    <div className="App">
+      <div className="topbar">
+			    <div className="container">
+				    <div className="social-links">
+					    <NavLink to="/login" className="login">로그인</NavLink> 
+					    <NavLink to="/join" className="join">회원가입</NavLink>
+				    </div>
+			    </div>
+		  </div>
+
+      <nav className="nav-menu">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <div className="navbar-brand">
+                <NavLink exact to="/"><i className="fas fa-book-reader"></i>JIKJI</NavLink>
+              </div>
+              <ul className="navbar-nav">
+                <li><NavLink to="viewer">VIEWER</NavLink></li>
+                <li><NavLink to="mypage">마이페이지</NavLink></li>
+                <li><NavLink to="search">검색 및 조회</NavLink></li>
+                <li><NavLink to="statistics">통계자료</NavLink></li>
+              </ul>
+            </div>
+          </div>
+      </nav>
+
+      <div className="contents">
+        <br />
+        <h4>새로 업데이트 된 책</h4>
+        <hr />
+
+        <div className="container">
+            {
+              books.map((a, i)=>{
+                return <Book books={books[i]} />
+              })
+            }
+        </div>
+
+      </div>
+
+    </div>
   );
 }
+
+  function Book(props) {
+    return(
+      <div className="col-md-2.4">
+        <img src="https://www.gutenberg.org/cache/epub/66209/pg66209.cover.medium.jpg" alt="book"/>
+        <h5>{props.books.title}</h5>
+      </div>
+    );
+  }
 
 /* class App extends React.Component {
   state = {
---
+++ frontend/src/Newbook.js
@@ -0,0 +1,24 @@
+/*eslint-disable*/
+
+export default[
+    {
+        id: 0,
+        title: "To Sup With the Devil"
+    },
+    {
+        id: 1,
+        title: "A Prose English Translation Of Vishupuranam"
+    },
+    {
+        id: 2,
+        title: "The Journal of the American-Irish Historical Society (Vol. VII)"
+    },
+    {
+        id: 3,
+        title: "Frank Reade, Jr., With His New Steam Man in Central America"
+    },
+    {
+        id: 4,
+        title: "Literary Pilgrimages of a Naturalist"
+    }
+]
