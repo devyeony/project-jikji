c5fecd78e4d74cde450e8297dc1b17d0700512d3
devyeony <devyeony@gmail.com>
2021-09-04 14:26:28 +0900

    chore #19 - 배포용 쉘 스크립트 jar 파일 실행 부분 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,7 +28,9 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \ 
+	-Dspring.config.location=classpath:\application.properties \ 
+	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 echo "> nginx 재시작"
 sudo service nginx restart
