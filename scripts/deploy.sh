4422068b08ff4b0e28b63f8d7add7cd4eb118f02
devyeony <devyeony@gmail.com>
2021-09-04 15:17:09 +0900

    chore #19 - 배포용 쉘 스크립트 jar 파일 실행 부분 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,15 +5,18 @@ PROJECT_NAME=project-jikji
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+IDLE_PORT=7000
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-if [ -z "$CURRENT_PID" ]; then
+echo "> $IDLE_PORT에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+if [ -z "$IDLE_PID" ]; 
+then
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID" 
-    kill -15 $CURRENT_PID 
+    echo "> kill -15 $IDLE_PID"
+    kill -15 ${IDLE_PID} 
     sleep 5
 fi
 
