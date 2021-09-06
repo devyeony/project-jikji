431456a91183e80a21652a45e0f5938040e4d3aa
devyeony <devyeony@gmail.com>
2021-09-06 11:46:07 +0900

    docs #22 - DDL 관련 SQL 파일 추가

---
+++ backend/jikji-core/src/docs/init-DDL.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Schema jikji
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `jikji` DEFAULT CHARACTER SET utf8 ;
+USE `jikji` ;
+
