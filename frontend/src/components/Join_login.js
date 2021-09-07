11baff6df328f600fddfbc1cbdc0ddf897080ea2
devminseo <dev.minseo@gmail.com>
2021-09-08 01:12:26 +0900

    style #24 로그인창 배경 추가 및 타이틀바 추가

--- frontend/src/components/Join_login.js
+++ frontend/src/components/Join_login.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './Join_login.css';
+// import styled from 'styled-components';
+// import palette from '../../lib/styles/palette';
 import { Button } from 'react-bootstrap';
 
 function Join_login() {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="join_login">
+    <div className="container-fluid">
+        <div className="row">
+            <div className="join_login">
+                <div className="white_box">
+                <div className="title"><i className="fas fa-book-reader"></i>로그인 및 회원가입</div>
+                <div className="list">
                     <Button variant="info">로그인</Button>
                     <Button varient="primary">회원가입</Button>
                 </div>
-            </div>
-            <div className="flatform">
-                <Button style={{ backgroundColor:'#02C73B' }}>네이버 계정으로 로그인</Button><br />
-                <Button style={{ backgroundColor: '#EF4545' }}>구글 계정으로 로그인</Button>
+                <div className="flatform">
+                    <Button style={{ backgroundColor:'#02C73B' }}>네이버 계정으로 로그인</Button><br />
+                    <Button style={{ backgroundColor: '#EF4545' }}>구글 계정으로 로그인</Button>
+                </div>
+                </div>
             </div>
         </div>
+    </div>
     );
 }
 
