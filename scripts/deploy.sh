4a72feb4c86d32f1162c340a2c4127cc7e6e6d16
devyeony <devyeony@gmail.com>
2021-09-07 20:29:20 +0900

    chore #16 - gitignore 처리된 application.yml을 서버에 업로드하고 배포 쉘 스크립트에 classpath 지정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:\application.properties $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:\home\ec2-user\app\application.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 echo "> nginx 재시작"
 sudo service nginx restart
