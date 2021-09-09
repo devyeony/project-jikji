b53873f5ea6a0e826490f484023358c1067aca0a
devminseo <dev.minseo@gmail.com>
2021-09-10 00:34:22 +0900

    feat #31 - 색인 검색창 추가

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Search.css';
+import Author from './Author.js';
+import Book from './Book.js';
+import { Link } from 'react-router-dom';
 
 function Search() {
+
+    let[name, chName] = useState(Author);
+    let[title, chTitle] = useState(Book);
+    
     return (
-        <div>검색 및 조회창</div>
+    <>
+        <div className="author">
+            작가 검색{' '}:{' '}
+            {
+                name.map((a, i)=>{
+                    return(
+                        <AuthorName name={name[i]} key={i}/>
+                    );
+                })
+            }
+        </div>
+        <div className="book">
+            책 검색{' '}:{' '}
+            {
+                title.map((a, i)=>{
+                    return(
+                        <BookName title={title[i]} key={i}/>
+                    );
+                })
+            }
+        </div>
+    </>
     );
 }
+
+function AuthorName(props) {
+    return(
+        <>
+        <span className="authorName">
+            <Link to={props.name.link}>{props.name.list}</Link>
+        </span>
+        </>
+    )
+}
+
+function BookName(props) {
+    return(
+        <>
+        <span className="bookName">
+            <Link to={props.title.link}>{props.title.list}</Link>
+        </span>
+        </>
+    )
+}
+
+
+
+
 
 export default Search;
