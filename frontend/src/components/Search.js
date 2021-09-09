d687b9f8c9256afd098cd6ef9564d633f1c76fa7
devminseo <dev.minseo@gmail.com>
2021-09-10 02:15:16 +0900

    feat #31 - 드롭다운, 검색창 추가

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Search.css';
 import Author from './Author.js';
 import Book from './Book.js';
+import { DropdownButton, Dropdown, Button, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Search() {
@@ -30,6 +31,18 @@ function Search() {
                     );
                 })
             }
+        </div>
+        <br />
+        <div className="container">
+            <div className="row">
+                <DropdownButton variant="outline-secondary" title="검색항목">
+                    <Dropdown.Item href="#/action-1">책제목</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">작가명</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">책내용</Dropdown.Item>
+                </DropdownButton>
+                <FormControl placeholder="검색어를 입력하세요."/>
+                <Button variant="primary">검색</Button>
+            </div>
         </div>
     </>
     );
