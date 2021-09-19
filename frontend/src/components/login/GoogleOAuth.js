12d4cd99194f2ac54651071844b68dd1ace5c43e
devyeony <devyeony@gmail.com>
2021-09-20 03:53:05 +0900

    style #29 - OAuth 로그인 css 수정

--- frontend/src/components/login/GoogleOAuth.js
+++ frontend/src/components/login/GoogleOAuth.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
-// import Googlelogo from '../../assets/img/login/googlelogo.JPG';
 import GoogleLogin from 'react-google-login';
 import axios from "axios";
+import Googlelogo from '../../assets/img/login/googlelogo.JPG';
 
 const oauth = {
-    appUrl : process.env.REACT_APP_URL,
-    apiUrl : process.env.REACT_APP_API_URL,
-    authPath : "/oauth/jwt",
-    googleKey : process.env.REACT_APP_GOOGLE_KEY
+    appUrl: process.env.REACT_APP_URL,
+    apiUrl: process.env.REACT_APP_API_URL,
+    authPath: "/oauth/jwt",
+    googleKey: process.env.REACT_APP_GOOGLE_KEY
 }
 
 const config = {
@@ -21,28 +21,47 @@ class GoogleOAuth extends Component {
         return (
             <GoogleLogin
                 clientId={oauth.googleKey}
-                buttonText="구글 계정으로 로그인"
                 uxMode={'redirect'}
                 redirectUri={oauth.appUrl}
                 onSuccess={successGoogleLogin}
                 onFailure={failLogin}
                 cookiePolicy={'single_host_origin'}
                 isSignedIn={true}
+                render={renderProps => (
+                    <button onClick={renderProps.onClick} disabled={renderProps.disabled} style={{
+                        background: "white",
+                        border: "1px solid #ABABAB",
+                        borderRadius: "5px",
+                        color: "dimgrey",
+                        width: "277px",
+                        height: "60px"
+                    }}>
+                        <img src={Googlelogo} className="googleLogo" alt="googleLogo" />
+                        <span style={{
+                            fontSize: "17px", float: "right", marginTop: "5px", marginRight: "22px"
+                        }}>
+                            구글 아이디로 로그인
+                        </span>
+                    </button>
+                )}
             />
         )
     }
 }
 
-const successGoogleLogin = async (response) => {
-    let rData = await axios.post(
+
+const successGoogleLogin = async (googleResponse) => {
+    //console.log(response);
+    await axios.post(
         oauth.apiUrl + oauth.authPath + "/google",
-        JSON.stringify(response),
+        JSON.stringify(googleResponse),
         config
-    );
-    setItem(rData);
+    ).then((response) => {
+        setItem(response);
+    });
 };
 
-function setItem(rData){
+function setItem(rData) {
     if (rData.status === 200) {
         //console.log(rData.data);
         sessionStorage.setItem("userInfo", JSON.stringify(rData.data.userInfo));
@@ -51,7 +70,7 @@ function setItem(rData){
 }
 
 const failLogin = () => {
-    alert("로그인에 실패했습니다.");
+    console.log("로그인에 실패했습니다.");
 }
 
 export default GoogleOAuth;
