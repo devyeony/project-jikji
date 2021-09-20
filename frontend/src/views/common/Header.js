740b449083cf281f0155704be09a6b35cf5b9cac
devyeony <devyeony@gmail.com>
2021-09-21 00:18:02 +0900

    feat #29 - Header.js에서 사용하지 않는 GoogleLogout import 삭제

--- frontend/src/views/common/Header.js
+++ frontend/src/views/common/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { GoogleLogout } from 'react-google-login';
 import Cookies from 'universal-cookie';
 import '../../assets/css/common/Header.css';
 import Topbar from "../../components/common/Topbar";
