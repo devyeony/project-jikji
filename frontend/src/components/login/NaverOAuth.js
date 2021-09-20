87205a675937dbadeaf54a05ed2e1a51f5574936
devyeony <devyeony@gmail.com>
2021-09-20 16:16:11 +0900

    Revert "style #29 - OAuth 로그인 css 수정"
    This reverts commit 12d4cd99194f2ac54651071844b68dd1ace5c43e.

--- frontend/src/components/login/NaverOAuth.js
+++ frontend/src/components/login/NaverOAuth.js
@@ -1,60 +1,44 @@
-import React, {useEffect} from "react";
+import React, {Component} from "react";
 import Naverlogo from '../../assets/img/login/naverlogo.JPG';
-import axios from "axios";
+import {Button} from "react-bootstrap";
+// import axios from "axios";
 
-const oauth = {
-    appUrl : process.env.REACT_APP_URL,
-    apiUrl : process.env.REACT_APP_API_URL,
-    authPath : "/oauth/jwt",
-    naverKey : process.env.REACT_APP_NAVER_KEY
-}
+// const oauth = {
+//     appUrl : process.env.REACT_APP_URL,
+//     apiUrl : process.env.REACT_APP_API_URL,
+//     authPath : "/oauth/jwt",
+//     naverKey : process.env.REACT_APP_NAVER_KEY
+// }
 
-const config = {
-    headers: {
-        "Content-Type": "application/json; charset=utf-8",
-    },
-};
+// const config = {
+//     headers: {
+//         "Content-Type": "application/json; charset=utf-8",
+//     },
+// };
 
-function NaverOAuth() {
-    const {naver} = window;
-
-    const Login = () => {
-        Naver();
-    };
-
-    useEffect(Login, []);
-
-    function Naver() {
-        var naverLogin = new naver.LoginWithNaverId({
-            clientId: oauth.naverKey,
-            callbackUrl: oauth.appUrl,
-            isPopup: false,
-            loginButton: {color: 'green', type: 3, height: 60} /* 로그인 버튼의 타입을 지정 */,
-            callbackHandle: true,
-            onSuccess: {successNaverLogin}
-        });
-
-        naverLogin.init();
+class NaverOAuth extends Component {
+    render() {
+        return (
+            <div>
+                <Button style={{ backgroundColor:'#02C73B' }}>
+                    <img src={Naverlogo} className="naver" alt="naver" />
+                    네이버 계정으로 로그인
+                </Button>
+            </div>
+        )
     }
-
-    /* style={{backgroundColor: '#02C73B'}}
-            <img src={Naverlogo} className="naver" alt="naver"/> */
-
-    return (
-        <div id="naverIdLogin" onClick={Login}></div>
-    );
 }
 
-const successNaverLogin = async (response) => {
-    console.log(response);
-    // let rData = await axios.post(
-    //     oauth.apiUrl + oauth.authPath + "/google",
-    //     JSON.stringify(response),
-    //     config
-    // );
-    // setItem(rData);
-};
-
+// const successNaverLogin = async (response) => {
+//     console.log(response);
+//     // let rData = await axios.post(
+//     //     oauth.apiUrl + oauth.authPath + "/google",
+//     //     JSON.stringify(response),
+//     //     config
+//     // );
+//     // setItem(rData);
+// };
+//
 // function setItem(rData){
 //     if (rData.status === 200) {
 //         //console.log(rData.data);
