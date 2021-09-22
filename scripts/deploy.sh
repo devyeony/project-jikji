c554bbbe159b42f72cbea102dd8db95dd2445b42
devyeony <devyeony@gmail.com>
2021-09-22 17:48:49 +0900

    chore #19 - 배치 모듈 추가로 인한 배포용 쉘 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,33 +5,59 @@ PROJECT_NAME=project-jikji
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-IDLE_PORT=7000
+JIKJI_WEB_PORT=7000
+echo "> $JIKJI_WEB_PORT에서 구동중인 애플리케이션 pid 확인"
+JIKJI_WEB_PID=$(lsof -ti tcp:${JIKJI_WEB_PORT})
 
-echo "> $IDLE_PORT에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
-
-echo "> 현재 구동 중인 애플리케이션 pid: $IDLE_PID"
-if [ -z "$IDLE_PID" ]; 
+echo "> 현재 구동 중인 JIKJI_WEB 애플리케이션 pid: $JIKJI_WEB_PID"
+if [ -z "$JIKJI_WEB_PID" ]; 
 then
-    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동 중인 JIKJI_WEB 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $IDLE_PID"
-    kill -15 ${IDLE_PID} 
+    echo "> kill -15 $JIKJI_WEB_PID"
+    kill -15 ${JIKJI_WEB_PID} 
     sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+echo "> JIKJI_WEB_JAR 배포"
+JIKJI_WEB_JAR=$(ls -tr $REPOSITORY/jikji-web-*.jar | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
+echo "> JIKJI_WEB_JAR: $JIKJI_WEB_JAR"
 
-echo "> $JAR_NAME 에 실행권한 추가"
+echo "> $JIKJI_WEB_JAR 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x $JIKJI_WEB_JAR
 
-echo "> $JAR_NAME 실행"
+echo "> $JIKJI_WEB_JAR 실행"
 
-nohup java -jar -Dspring.profiles.active=dev $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=dev $JIKJI_WEB_JAR > $REPOSITORY/nohup-jikji-web.out 2>&1 &
+
+JIKJI_BATCH_PORT=9000
+echo "> $JIKJI_BATCH_PORT에서 구동중인 애플리케이션 pid 확인"
+JIKJI_BATCH_PID=$(lsof -ti tcp:${JIKJI_BATCH_PORT})
+
+echo "> 현재 구동 중인 JIKJI_BATCH 애플리케이션 pid: $JIKJI_BATCH_PID"
+if [ -z "$JIKJI_BATCH_PID" ]; 
+then
+    echo "> 현재 구동 중인 JIKJI_BATCH 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $JIKJI_BATCH_PID"
+    kill -15 ${JIKJI_BATCH_PID} 
+    sleep 5
+fi
+
+echo "> JIKJI_BATCH_JAR 배포"
+JIKJI_BATCH_JAR=$(ls -tr $REPOSITORY/jikji-batch-*.jar | tail -n 1)
+
+echo "> JIKJI_BATCH_JAR: $JIKJI_BATCH_JAR"
+
+echo "> $JIKJI_BATCH_JAR 에 실행권한 추가"
+
+chmod +x $JIKJI_BATCH_JAR
+
+echo "> $JIKJI_BATCH_JAR 실행"
+
+nohup java -jar -Dspring.profiles.active=dev $JIKJI_BATCH_JAR > $REPOSITORY/nohup-jikji-batch.out 2>&1 &
 
 echo "> nginx 재시작"
 sudo service nginx restart
